b5628e94cf9e1762b75e35f4c6e49ad7d78f3a40
Ashish <ashaman19262@gmail.com>
2025-06-17 21:31:40 +0530

    Used the value annotation in the weatherService

--- src/main/java/com/example/JournalsAndAuthors/service/WeatherService.java
+++ src/main/java/com/example/JournalsAndAuthors/service/WeatherService.java
@@ -2,6 +2,7 @@ package com.example.JournalsAndAuthors.service;
 
 import com.example.JournalsAndAuthors.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,8 +10,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    private static final String apiKey = "62e88c45e13800fde6794f825fb47bc0";
-    private static final String API = "https://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+    @Value("${weather.service.apikey}")
+    private String apiKey;
+    @Value("${weather.service.url}")
+    private String API;
 
     @Autowired
     private RestTemplate restTemplate;
